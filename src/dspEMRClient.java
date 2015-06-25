958ec783189e47f8157b5de1b8af4c3658517afa
rancdg <rancdg@gmail.com>
2015-06-25 03:32:15 +0300

    emr client

--- src/dspEMRClient.java
+++ src/dspEMRClient.java
@@ -17,7 +17,7 @@ import com.amazonaws.services.elasticmapreduce.model.StepConfig;
 
 public class dspEMRClient {
 
-	public static String propertiesFilePath = "cred.properties";
+	public static String propertiesFilePath = "/home/ran/Downloads/cred.properties";
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException{
 	
@@ -25,9 +25,9 @@ public class dspEMRClient {
 	    AmazonElasticMapReduce mapReduce = new AmazonElasticMapReduceClient(credentials);
 	    mapReduce.setRegion(Region.getRegion(Regions.US_EAST_1));
 	    HadoopJarStepConfig hadoopJarStep = new HadoopJarStepConfig()
-	        .withJar("s3n://ranerandsp2/DSP2.jar") // This should be a full map reduce application.
+	        .withJar("s3n://ranerandsp2/dsp2.jar") // This should be a full map reduce application.
 	        .withMainClass("NgramGeneral.Ncount")
-	        .withArgs("s3n://dsp152/output/eng-us-all-100k-2gram", "s3n://ranerandsp2/output/out" , "0.5" , "0.2" , "eng" , "0");
+	        .withArgs("s3://datasets.elasticmapreduce/ngrams/books/20090715/eng-us-all/2gram/data", "s3n://ranerandsp2/output/out" , "0.5" , "0.2" , "eng" , "0");
 	     
 	    StepConfig stepConfig = new StepConfig()
 	        .withName("ncount")
@@ -37,17 +37,17 @@ public class dspEMRClient {
 	    
 	    JobFlowInstancesConfig instances = new JobFlowInstancesConfig()
 	        .withInstanceCount(2)
-	        .withMasterInstanceType(InstanceType.M1Medium.toString())
-	        .withSlaveInstanceType(InstanceType.M1Medium.toString())
-	        .withHadoopVersion("2.2.0").withEc2KeyName("raneran")
+	        .withMasterInstanceType(InstanceType.M1Large.toString())
+	        .withSlaveInstanceType(InstanceType.C3Xlarge.toString())
+	        .withHadoopVersion("2.4.0").withEc2KeyName("raneran")
 	        .withKeepJobFlowAliveWhenNoSteps(false);
-//	        .withPlacement(new PlacementType("us-east-1a"));
+	        //.withPlacement(new PlacementType("us-east-1a"));
 	     
 	    RunJobFlowRequest runFlowRequest = new RunJobFlowRequest()
 	    .withAmiVersion("latest")
 	    .withServiceRole("EMR_DefaultRole")
 	    .withJobFlowRole("EMR_EC2_DefaultRole")
-	    .withName("Example")
+	    .withName("DSP2flow")
 	        .withInstances(instances)
 	        .withSteps(stepConfig)
 	        .withLogUri("s3n://ranerandsp2/logs/");
